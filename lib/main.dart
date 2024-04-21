d5da2cf2145787a3d396cf5db1b03bd44d4646d9
Fabian Schipp <fschipp@slashwhy.de>
2024-04-21 13:29:03 +0200

    version 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,151 @@
+import 'package:flutter/cupertino.dart';
+import 'package:native_exif/native_exif.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const GeoTagger());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GeoTagger extends StatelessWidget {
+  const GeoTagger({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GeoTagger',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPageState extends State<MainPage> {
+  final _picker = ImagePicker();
+  String _result = "Tap to tag";
+  bool _isLoading = false;
 
-  void _incrementCounter() {
+  Future<Position> _determinePosition() async {
+    var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw ('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw ('Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void _setLoadingState(bool isLoading) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isLoading = isLoading;
     });
+  }
+
+  Future _tagImage(BuildContext context) async {
+    if (_isLoading) {
+      return false;
+    }
+
+    _setLoadingState(true);
+
+    final pickerImage =
+        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 100);
+
+    if (pickerImage == null) {
+      _result = "Tap to tag";
+      _setLoadingState(false);
+      return;
+    }
+
+    Position? position;
+    try {
+      position = await _determinePosition();
+    } catch (e) {
+      debugPrint(e.toString());
+      _result = "Error";
+      _setLoadingState(false);
+      return;
+    }
+
+    var exif = await Exif.fromPath(pickerImage.path);
+
+    final lat = position.latitude;
+    final long = position.longitude;
+
+    await exif.writeAttributes({
+      'GPSLatitude': lat,
+      'GPSLatitudeRef': lat > 0 ? "N" : "S",
+      'GPSLongitude': long,
+      'GPSLongitudeRef': long > 0 ? "E" : "W",
+      'GPSAltitude': position.altitude
+    });
+
+    await exif.close();
+
+    _result = "Tagged to\n(${position.latitude}, ${position.longitude})";
+    _setLoadingState(false);
+
+    await Share.shareXFiles([XFile(pickerImage.path)]);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+        child: MaterialButton(
+          onPressed: () async => await _tagImage(context),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 12),
+                    child: Image.asset(
+                      "assets/pin.png",
+                      alignment: Alignment.center,
+                      width: 82,
+                    ),
+                  ),
+                  _isLoading
+                      ? const SizedBox(
+                          height: 48,
+                          width: 48,
+                          child: CircularProgressIndicator(color: Colors.black),
+                        )
+                      : SizedBox(
+                          height: 48,
+                          child: Text(_result,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(fontSize: 18)),
+                        ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
