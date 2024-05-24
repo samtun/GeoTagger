ede9b5a0f70570f582b4cc044f801abba573a409
samtun <schipp@nerrix.com>
2024-05-24 11:12:49 +0200

    add support for multi image selection

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 import 'package:geo_tagger/di.dart';
 import 'package:geo_tagger/map_page.dart';
 import 'package:geo_tagger/repositories/position_repository.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:native_exif/native_exif.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
@@ -43,7 +43,8 @@ class _MainPageState extends State<MainPage> {
 
   bool _isLoading = false;
 
-  late final Future<bool> _locationCanBeFetched = _positionRepository.locationCanBeFetched();
+  late final Future<bool> _locationCanBeFetched =
+      _positionRepository.locationCanBeFetched();
 
   void _setLoadingState(bool isLoading) {
     setState(() {
@@ -51,57 +52,64 @@ class _MainPageState extends State<MainPage> {
     });
   }
 
-  Future _tagImageToCurrentPosition() async {
+  Future _tagImageGeoData(bool useCurrentPosition) async {
     if (_isLoading) {
       return false;
     }
 
     _setLoadingState(true);
 
-    final pickerImage =
-        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 100);
+    // Pick images
+    final pickerImages = await _picker.pickMultiImage(imageQuality: 100);
 
-    if (pickerImage == null) {
+    if (pickerImages.isEmpty) {
       _setLoadingState(false);
       return;
     }
 
-    Position? position;
-    try {
-      position = await _positionRepository.getCurrentPosition();
-    } catch (e) {
-      debugPrint(e.toString());
+    // Get Geo data
+    final position = await _getGeoData(useCurrentPosition);
+
+    if (position == null) {
       _setLoadingState(false);
       return;
     }
 
-    var exif = await Exif.fromPath(pickerImage.path);
+    // Write Geo data
+    for (final image in pickerImages) {
+      var exif = await Exif.fromPath(image.path);
 
-    final lat = position.latitude;
-    final long = position.longitude;
+      final lat = position.latitude;
+      final long = position.longitude;
 
-    await exif.writeAttributes({
-      'GPSLatitude': lat,
-      'GPSLatitudeRef': lat > 0 ? "N" : "S",
-      'GPSLongitude': long,
-      'GPSLongitudeRef': long > 0 ? "E" : "W"
-    });
+      await exif.writeAttributes({
+        'GPSLatitude': lat,
+        'GPSLatitudeRef': lat > 0 ? "N" : "S",
+        'GPSLongitude': long,
+        'GPSLongitudeRef': long > 0 ? "E" : "W"
+      });
 
-    await exif.close();
+      await exif.close();
+    }
+
+    // Save images
+    await Share.shareXFiles(pickerImages);
 
     _setLoadingState(false);
-
-    await Share.shareXFiles([XFile(pickerImage.path)]);
   }
 
-  Future _tagImageToMapLocation() async {
-    if (_isLoading) {
-      return false;
+  Future<LatLng?> _getGeoData(bool useCurrentPosition) async {
+    if (useCurrentPosition) {
+      try {
+        final position = await _positionRepository.getCurrentPosition();
+        return LatLng(position.latitude, position.longitude);
+      } catch (e) {
+        debugPrint(e.toString());
+        return null;
+      }
     }
 
-    _setLoadingState(true);
-
-    var position = await Navigator.push(
+    final mapLocation = await Navigator.push(
       context,
       PageRouteBuilder(
         pageBuilder: (context, animation1, animation2) => const MapPage(),
@@ -109,35 +117,7 @@ class _MainPageState extends State<MainPage> {
       ),
     );
 
-    if (position == null) {
-      _setLoadingState(false);
-      return;
-    }
-
-    final pickerImage = await _picker.pickImage(source: ImageSource.gallery, imageQuality: 100);
-
-    if (pickerImage == null) {
-      _setLoadingState(false);
-      return;
-    }
-
-    var exif = await Exif.fromPath(pickerImage.path);
-
-    final lat = position.latitude;
-    final long = position.longitude;
-
-    await exif.writeAttributes({
-      'GPSLatitude': lat,
-      'GPSLatitudeRef': lat > 0 ? "N" : "S",
-      'GPSLongitude': long,
-      'GPSLongitudeRef': long > 0 ? "E" : "W"
-    });
-
-    await exif.close();
-
-    _setLoadingState(false);
-
-    await Share.shareXFiles([XFile(pickerImage.path)]);
+    return LatLng(mapLocation.latitude, mapLocation.longitude);
   }
 
   @override
@@ -152,63 +132,68 @@ class _MainPageState extends State<MainPage> {
               fit: BoxFit.cover,
             ),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 16),
-                    child: Image.asset(
-                      "assets/pin.png",
-                      alignment: Alignment.center,
-                      width: 82,
-                    ),
-                  ),
-                  FutureBuilder<bool>(
-                    future: _locationCanBeFetched,
-                    builder: (context, snapshot) => MaterialButton(
-                      onPressed: snapshot.data == true ? _tagImageToCurrentPosition : null,
-                      color: Colors.white,
-                      disabledColor: Colors.white,
-                      disabledElevation: 1,
-                      padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-                      child: const Row(
-                        children: [
-                          Icon(Icons.location_on),
-                          SizedBox(width: 6),
-                          Text(
-                            "Use my position",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  MaterialButton(
-                    onPressed: _tagImageToMapLocation,
-                    color: Colors.white,
-                    padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-                    child: const Row(
+          child: _isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(color: Colors.black))
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Icon(Icons.map),
-                        SizedBox(width: 6),
-                        Text(
-                          "Use map location",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 18),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 16),
+                          child: Image.asset(
+                            "assets/pin.png",
+                            alignment: Alignment.center,
+                            width: 82,
+                          ),
+                        ),
+                        FutureBuilder<bool>(
+                          future: _locationCanBeFetched,
+                          builder: (context, snapshot) => MaterialButton(
+                            onPressed: () => snapshot.data == true
+                                ? _tagImageGeoData(true)
+                                : null,
+                            color: Colors.white,
+                            disabledColor: Colors.white,
+                            disabledElevation: 1,
+                            padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                            child: const Row(
+                              children: [
+                                Icon(Icons.location_on),
+                                SizedBox(width: 6),
+                                Text(
+                                  "Use my position",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        MaterialButton(
+                          onPressed: () => _tagImageGeoData(false),
+                          color: Colors.white,
+                          padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                          child: const Row(
+                            children: [
+                              Icon(Icons.map),
+                              SizedBox(width: 6),
+                              Text(
+                                "Use map location",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                  ],
+                ),
         ),
       ),
     );
